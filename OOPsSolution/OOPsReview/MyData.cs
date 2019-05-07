51cd37c78405c5a4c5ab0b217a331d414008cd56
RPoworoznik <50344460+RPoworoznik@users.noreply.github.com>
2019-05-06 18:48:52 -0600

    06May2019

---
+++ OOPsSolution/OOPsReview/MyData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    //The default access permissions are private
+    //If an outside user of this class wanted access to the class contents, then the access permissions need to be public
+    //If this class was static, the class would not hold data
+    //For a class to hold data, do not make it static, unless you want everyone to have access to that data (read and change)
+
+    public class myData
+    {
+        public int Height;
+        public int Width;
+
+    }
+}
--- OOPsSolution/OOPsReview/Program.cs
+++ OOPsSolution/OOPsReview/Program.cs
@@ -9,8 +9,32 @@ namespace OOPsReview
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+        {   
+            //Console is a reference to a system static class
+            //Static classes are used by typing the class name 
+            //Static classes do not store variable data
+            //Static classes are used by everyone (are public and not private)
+            //Methods of a static class will be placed after the object dot operator
+            Console.WriteLine("Hello World");
+
+            //Create an instane of a non-static class
+            //**An object is a programmer/developer defined datatype
+            //If one creates a declarative using just the class name, then this is only a "pointer" that can hold an instance of a class specified
+            //Current value of aVariabel is "null"
+            myData aVariable = null;
+
+            //If you use the "new" command with a class, the "new" command will "physically" create and instance of the class
+            myData anInstance = new myData();
+            if (aVariable == null)
+            {
+                Console.WriteLine("aVariable has nothing in it");   
+            }
+            if (anInstance == null)
+            {
+                Console.WriteLine("anInstance has nothing in it");               
+            }
+            Console.ReadLine();
+        }//eom
+    }///eoc
+
+}//eon
